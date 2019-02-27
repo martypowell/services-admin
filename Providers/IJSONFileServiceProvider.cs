96a73c29b478ae12163d8a6f745f94809a60d422
martypowell <jpowel25@gmail.com>
2019-02-27 12:53:25 -0500

    added skeleton for a json file provider

---
+++ Providers/IJSONFileServiceProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using services.Models;
+
+namespace services.Providers
+{
+    public interface IJSONFileServiceProvider
+    {
+        IEnumerable<Service> GetServices();
+        bool AddService(
+            Service Service);
+    }
+}
---
+++ Providers/JSONFileServiceProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using services.Models;
+
+namespace services.Providers
+{
+    public class JSONFileServiceProvider : IJSONFileServiceProvider
+    {
+        public IEnumerable<Service> GetServices()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AddService(
+            Service Service)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
