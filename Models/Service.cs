3c6ce802dee8dca288b00f85b2ac6b08df72e33d
Marty Powell <jpowel25@gmail.com>
2019-02-26 21:02:46 -0500

    added model and controller for services

---
+++ Models/Service.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace services.Models
+{
+    public class Service
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Agency { get; set; }
+        public IEnumerable<string> Keywords { get; set; }
+        public bool IsMostPopularService { get; set; }
+    }
+}
