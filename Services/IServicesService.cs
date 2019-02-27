a60f92f0abc7ffbd4882c3072b21a39d0253389b
Marty Powell <jpowel25@gmail.com>
2019-02-26 21:30:56 -0500

    added method for getting a service by id

--- Services/IServicesService.cs
+++ Services/IServicesService.cs
@@ -5,7 +5,23 @@ namespace services.Services
 {
     public interface IServicesService
     {
+        /// <summary>
+        /// Gets a List of Services
+        /// </summary>
+        /// <returns></returns>
         IEnumerable<Service> GetServices();
+
+        /// <summary>
+        /// Get a Service by it's service id
+        /// </summary>
+        /// <param name="id">Primary Key associated with the service.</param>
+        /// <returns></returns>
+        Service GetService(int id);
+
+        /// <summary>
+        /// Save a Service
+        /// </summary>
+        /// <returns>Returns the saved service with new or updated values.</returns>
         Service SaveService();
     }
 }
