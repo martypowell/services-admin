ee79b84f2b37379c6497117f98c83fbe6a70f056
Marty Powell <jpowel25@gmail.com>
2019-02-27 19:27:54 -0500

    added ability to add service

--- Providers/JSONFileServiceProvider.cs
+++ Providers/JSONFileServiceProvider.cs
@@ -13,23 +13,35 @@ namespace services.Providers
     public class JSONFileServiceProvider : IServicesProvider
     {
         private static readonly string dataFilePath = "./data/services.json";
-        public IEnumerable<Service> GetServices()
-        {
-            var servicesAsJson = GetServicesAsJson();
 
-            return servicesAsJson.ToObject<IEnumerable<Service>>();
-        }
+        public IEnumerable<Service> GetServices() =>
+            GetServicesFromFile();
 
         public Service AddService(
-            Service Service)
+            Service service)
         {
-            throw new NotImplementedException();
+            var services = GetServices().ToList();
+            var newId = services.Max(x => x.Id) + 1;
+
+            service.Id = newId;
+
+            services.Add(service);
+
+            File.WriteAllText(dataFilePath, JsonConvert.SerializeObject(services));
+
+            return service;
         }
 
-        private dynamic GetServicesAsJson()
+        private JArray GetServicesAsJson()
         {
             var fileConent = File.ReadAllText(dataFilePath);
             return JArray.Parse(fileConent);
+        }
+
+        private IEnumerable<Service> GetServicesFromFile()
+        {
+            var jsonArray = GetServicesAsJson();
+            return jsonArray.ToObject<IEnumerable<Service>>();
         }
     }
 }
