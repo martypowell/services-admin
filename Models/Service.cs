49379e3aa0fdb40cd10d240b1015977743c5facd
Marty Powell <jpowel25@gmail.com>
2019-03-07 21:16:00 -0500

    added aws attributes

--- Models/Service.cs
+++ Models/Service.cs
@@ -1,9 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using Amazon.DynamoDBv2.DataModel;
+using System.Collections.Generic;
 
 namespace services.Models
 {
+    [DynamoDBTable("Services")]
     public class Service
     {
+        [DynamoDBHashKey]
         public int Id { get; set; }
         public string Name { get; set; }
         public string Agency { get; set; }
