c45e28e306dbde2722cf9d5f8a735135a170d5a2
Marty Powell <jpowel25@gmail.com>
2019-03-09 18:54:36 -0500

    added categories component

---
+++ ClientApp/src/components/Admin/Categories.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Keywords = props => (
+  <div className="keywords">
+    <h1>Categories</h1>
+    <p>
+      This is a list of the following Categories and how they are used in active
+      services.
+    </p>
+  </div>
+);
+
+export default Keywords;
