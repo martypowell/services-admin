be1d32f0c0299685fa7f64ff77796d8aafd20cc0
Marty Powell <jpowel25@gmail.com>
2019-03-10 16:23:16 -0400

    removed breadcrumb

--- ClientApp/src/components/Layout.js
+++ ClientApp/src/components/Layout.js
@@ -26,11 +26,6 @@ const AdminLayout = props => (
       </Menu>
     </Header>
     <Content style={{ padding: "0 50px" }}>
-      <Breadcrumb style={{ margin: "16px 0" }}>
-        <Breadcrumb.Item>Home</Breadcrumb.Item>
-        <Breadcrumb.Item>List</Breadcrumb.Item>
-        <Breadcrumb.Item>App</Breadcrumb.Item>
-      </Breadcrumb>
       <div style={{ background: "#fff", padding: 24, minHeight: 280 }}>
         {props.children}
       </div>
