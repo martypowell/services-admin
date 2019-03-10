457613fda0aafafe9496622123e92a6d199c66c5
Marty Powell <jpowel25@gmail.com>
2019-03-10 17:11:09 -0400

    added ad method

--- ClientApp/src/shared/Services.js
+++ ClientApp/src/shared/Services.js
@@ -13,4 +13,23 @@ const fetchServices = async callback => {
     .catch(console.error);
 };
 
-export { fetchServices };
+const addService = (service, authorization) => {
+  const postData = JSON.stringify(service);
+
+  return fetch("/api/service/0", {
+    method: "post",
+    headers: new Headers({
+      Authorization: `Bearer ${authorization.token}`,
+      "Content-Type": "application/json"
+    }),
+    body: postData
+  })
+    .then(response => {
+      return response
+        ? response.json()
+        : "Something went wrong getting the response";
+    })
+    .catch(console.error);
+};
+
+export { addService, fetchServices };
